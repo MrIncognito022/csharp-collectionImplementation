00a5947bc142aa8c8a7ede5798f29bbc44541998
Ahmed Qureshi <Mr.incognito022@gmail.com>
2022-04-07 14:38:22 -0700

    Added Switch statement in Program file that perform just 2 function 1)  Create an account 2)  Search an Account

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,7 +11,66 @@ namespace ConsoleApp
             //BankAccountHandler bankAccountHandler = new BankAccountHandler(100);
 
             IBankAccountHandler bankAccountHandler = new BankAccountCollectionHandler();
+            
             //IBankAccountHandler bankAccountHandler = new BankAccountHandler(100);
+
+            string title,accountType,code;
+
+            bool choice = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1...  Create a new Account");
+                Console.WriteLine("2.... Search an Existing Account");
+                string? option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Enter Account Title");
+                            title = Console.ReadLine().Trim();
+                            Console.WriteLine("Select the account type. 1) Saving 2) Current");
+                            accountType = Console.ReadLine().Trim();
+
+                            if (accountType == "1")
+                            {
+                                bankAccountHandler.Add(new SavingBankAccount()
+                                {
+                                    Title = title 
+                                });
+                            }
+                            else if (accountType == "2")
+                            {
+                                bankAccountHandler.Add(new CurrentBankAccount()
+                                { 
+                                    Title = title 
+                                }
+                                );
+                            }
+                            Console.WriteLine("Account created");
+                            Console.WriteLine("Press any key to continue");
+                            Console.ReadKey();
+                        }
+                        break;
+                    case "2":
+                        {
+                            Console.WriteLine("Enter Your Account Number");
+                            code = Console.ReadLine().Trim();
+                            BankAccount bankaccount = bankAccountHandler.GetBankAccount(code, false);
+                            Console.WriteLine(bankaccount.ToString());
+                            Console.ReadKey();
+                        }
+                        break;
+                    default:
+                        choice = false;
+                        break;
+                }
+            }
+            while (choice);
+
+
+
 
 
         }
